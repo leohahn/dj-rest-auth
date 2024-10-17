1794f8b480b78a5e0d744f6d809c3d7b5868263a
Leonardo Hahn <leonnardo.hahn@gmail.com>
2024-10-17 12:20:40 -0300

    feat: get pkce code verifier from correct place

--- dj_rest_auth/registration/serializers.py
+++ dj_rest_auth/registration/serializers.py
@@ -130,7 +130,9 @@ class SocialLoginSerializer(serializers.Serializer):
                 basic_auth=adapter.basic_auth,
             )
             try:
-                pkce_code_verifier = request.session.get("pkce_code_verifier")
+                from allauth.socialaccount.internal import statekit
+                state = statekit.unstash_last_state(request)
+                pkce_code_verifier = state.get("pkce_code_verifier")
                 token = client.get_access_token(code, pkce_code_verifier=pkce_code_verifier)
             except OAuth2Error as ex:
                 raise serializers.ValidationError(
